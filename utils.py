8ba2ef18aca9755b787f24cd55f23e81c9817ea9
Anil Yildiz <yildiz@stanford.edu>
2024-03-14 16:40:13 -0700

    np broadcasting

--- utils.py
+++ utils.py
@@ -18,6 +18,7 @@ def get_dji():
     soup = bs(request.text, "lxml")
     stats = soup.find('table',class_='tablepress tablepress-id-42 tablepress-responsive')
     df = pd.read_html(str(stats))[0]
+    print(f"DJI: Contains {len(df)} tickers.")
     return df
 
 def get_spy():
@@ -31,6 +32,7 @@ def get_spy():
     df['% Chg'] = pd.to_numeric(df['% Chg'])
     df['Chg'] = pd.to_numeric(df['Chg'])
     df = df.drop('#', axis=1)
+    print(f"SPY: Contains {len(df)} tickers.")
     return df
 
 def get_qqq():
@@ -55,6 +57,7 @@ def get_qqq():
     df = df.drop('Unnamed: 2', axis=1)
     df.rename(columns={'Ticker':'Symbol'},inplace=True)
     df = df.reset_index(drop=True)
+    print(f"QQQ: Contains {len(df)} tickers.")
     return df
 
 class NestedObject:
@@ -81,11 +84,15 @@ def get_dates(args):
         d = args.back_from_today.days
         m = args.back_from_today.months
         y = args.back_from_today.years
-        startdate = datetime.date.today()
-        enddate = startdate - relativedelta(days=d, months=m, years=y)
+        enddate = date.today()
+        startdate = enddate - relativedelta(days=d, months=m, years=y)
     else:
         ValueError("Incorrect value in args.yaml for method.")
     return startdate, enddate
+
+def get_pdr_data(tickers, startdate, enddate):
+    market_data = pdr.get_data_yahoo(tickers, start=startdate, end=enddate, progress=False)['Adj Close']
+    return market_data.sort_index()
 
 def get_benchmark_data(args):
     startdate, enddate = get_dates(args)
@@ -93,3 +100,42 @@ def get_benchmark_data(args):
     riskfree_data = pdr.get_data_yahoo(args.benchmark.riskfree, start=startdate, end=enddate, progress=False)['Adj Close']
     common_entries = set(market_data.keys()).intersection(set(riskfree_data.keys()))
     return market_data[common_entries].sort_index(), riskfree_data[common_entries].sort_index()
+
+def is_df_okay(df):
+    """ Returns True if there are no missing entries (NaN) in the df. """
+    return not df.isnull().any().any()
+
+def clean_df(df):
+    """ Remove columns that contain NaN in any row. """
+    # df = df.dropna(how='all', axis=1)
+    bad_cols = df.columns[df.isnull().any()]
+    return df.drop(bad_cols, axis=1)
+
+def compute_return(df, was_annual=False, retain_symbols=False):
+    if was_annual:
+        # Riskfree data should be converted to daily from annual.
+        riskfree_annual_return = (df/100)[1:]
+        riskfree_annual_log_return = np.log(1+riskfree_annual_return)
+        riskfree_daily_log_return = riskfree_annual_log_return/252
+        return riskfree_daily_log_return.to_numpy()
+    else:
+        # Stock market data is already retrieved as daily.
+        market_return = np.diff(np.log(df), axis=0)
+        if not retain_symbols:
+            return market_return
+        else:
+            return pd.DataFrame(market_return, columns=df.columns)
+    
+def sharpe_ratio(ticker_return_df, riskfree_return_df, retain_symbols=False):
+    """ Compute daily Sharpe ratio (what we use). """
+    if np.array(ticker_return_df).ndim == 1:
+        excess_return = ticker_return_df - riskfree_return_df
+    else:
+        excess_return = ticker_return_df - riskfree_return_df.reshape(-1, 1)
+    sharpe = excess_return.mean(axis=0) / excess_return.std(axis=0, ddof=1)
+    if not retain_symbols:
+        return np.array(sharpe)
+    elif retain_symbols and isinstance(ticker_return_df, np.ndarray):
+        return pd.DataFrame(sharpe, columns=ticker_return_df.columns)
+    else:  # is a pd.Series instance, and already has symbols retained.
+        return sharpe
